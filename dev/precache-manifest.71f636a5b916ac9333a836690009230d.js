4f0a6509f1371fcb555b481f5a52dd34d064fae9
Alyssa Burks <burksan@udel.edu>
2020-02-28 16:34:18 -0500

    Updates

---
+++ dev/precache-manifest.71f636a5b916ac9333a836690009230d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf1a513e95290479b69f6c9660a1a72",
+    "url": "/coding-2-try-phaser-burksan/devassets/.DS_Store"
+  },
+  {
+    "revision": "ea5fd491a642c241e0fdaf6d135abd39",
+    "url": "/coding-2-try-phaser-burksan/devassets/bears.png"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-burksan/devassets/citations.md"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/devassets/ice cream bar.png"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/devassets/ice cream bar1.png"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/devassets/ice cream bar2.png"
+  },
+  {
+    "revision": "a7b924b184180dae46130a522a6aa33f",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/.DS_Store"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/ship1.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-burksan/devassets/images/ship3.png"
+  },
+  {
+    "revision": "3425668310b6cfed7155db059c3563a9",
+    "url": "/coding-2-try-phaser-burksan/devassets/space.png"
+  },
+  {
+    "revision": "df31bca21db79f80f61cb4f7c34fb1a7",
+    "url": "/coding-2-try-phaser-burksan/devassets/spritesheets/.DS_Store"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-burksan/devassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-burksan/devassets/spritesheets/ship1.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-burksan/devassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-burksan/devassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "df74f95f9f8bbb57d74c",
+    "url": "/coding-2-try-phaser-burksan/devgen.main.a6c6c251703a7e3a51a0.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-burksan/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "49d2243d51c891eda2957df950251762",
+    "url": "/coding-2-try-phaser-burksan/devindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-burksan/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-burksan/devsw.js"
+  }
+]);
