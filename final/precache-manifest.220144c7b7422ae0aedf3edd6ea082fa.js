a9b0063404942c8e9a4f64c6135dae241cffedfb
Alyssa Burks <burksan@udel.edu>
2020-03-06 22:02:36 -0500

    Updates

---
+++ final/precache-manifest.220144c7b7422ae0aedf3edd6ea082fa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4941445336dc9edf5e3b2783a02e46f4",
+    "url": "/coding-2-try-phaser-burksan/finalassets/.DS_Store"
+  },
+  {
+    "revision": "ea5fd491a642c241e0fdaf6d135abd39",
+    "url": "/coding-2-try-phaser-burksan/finalassets/bears.png"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-burksan/finalassets/citations.md"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/finalassets/ice cream bar.png"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/finalassets/ice cream bar1.png"
+  },
+  {
+    "revision": "4115bd4304fde165c98870f27b32ead6",
+    "url": "/coding-2-try-phaser-burksan/finalassets/ice cream bar2.png"
+  },
+  {
+    "revision": "a7b924b184180dae46130a522a6aa33f",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/.DS_Store"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/ship1.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-burksan/finalassets/images/ship3.png"
+  },
+  {
+    "revision": "3425668310b6cfed7155db059c3563a9",
+    "url": "/coding-2-try-phaser-burksan/finalassets/space.png"
+  },
+  {
+    "revision": "563f18620818e1309d1b5c77cec0286f",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/.DS_Store"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "969b0828463c5159d61bc169ca93f4a2",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/icecream.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/ship1.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-burksan/finalassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "1326af481decde7fb749",
+    "url": "/coding-2-try-phaser-burksan/finalgen.main.0b6480d11ecb44a364eb.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-burksan/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "819819121f18cf515029d050cc48f6cf",
+    "url": "/coding-2-try-phaser-burksan/finalindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-burksan/finalmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-burksan/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-burksan/finalprecache-manifest.220144c7b7422ae0aedf3edd6ea082fa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
